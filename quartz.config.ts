52708c5f97a0177d9a4adcb213cfb87b8214593d
Mocoss <849172201@qq.com>
2026-08-02 13:47:08 +0800

    开启SPA路由，新增/content/_redirects文件

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "魔客室",
     pageTitleSuffix: "",
-    enableSPA: false,
+    enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
